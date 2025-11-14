643da9e85c09db31bd21b94652a800a1a977c72a
Lucca Panetta <panettalucca@gmail.com>
2025-11-14 15:18:50 -0300

    Configurar conexion a PostgreSQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Configuración de la Base de Datos 
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], 
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        
+        // Leemos las variables del .env
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        
+        autoLoadEntities: true, 
+
+        synchronize: true, 
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
