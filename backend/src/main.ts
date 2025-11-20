eb2424502a28a07f3972ee575b967c263d8a2ce5
Lucca Panetta <panettalucca@gmail.com>
2025-11-20 19:51:38 -0300

    feat(infra): implementar validacion global y filtro de excepciones

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-;
-import * as dotenv from 'dotenv';
-import { GlobalValidationPipe } from './infra/validators/pipes/global-validation.pipe';
+import { ValidationPipe } from '@nestjs/common'; 
 import { HttpExceptionFilter } from './infra/validators/http-exception.filter';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new GlobalValidationPipe());
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, 
+      forbidNonWhitelisted: true,
+      transform: true, 
+    }),
+  );
+
   app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT || 3000);
