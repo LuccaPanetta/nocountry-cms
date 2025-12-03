bcc0dcbce915561e33f53627b0d4a1fdae24b61c
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-12-03 10:27:32 -0300

    Ajustes css layout

--- frontend/testigo-app/src/components/layout/Footer.tsx
+++ frontend/testigo-app/src/components/layout/Footer.tsx
@@ -1,7 +1,8 @@
 
 const Footer = () => {
   return (
-    <footer className='mt-auto h-8 w-full bg-secondary'>      
+    <footer className='mt-auto h-8 w-full bg-secondary text-center text-xs md:text-sm' >   
+    <p className="pt-2">&copy; 2025 TestiGO. Todos los derechos reservados.</p>
     </footer>
   )
 }
--- frontend/testigo-app/src/components/layout/Header.tsx
+++ frontend/testigo-app/src/components/layout/Header.tsx
@@ -4,9 +4,9 @@ import Logo from "../ui/Logo"
 const Header = () => {
   return (
     <header className="h-25 w-full shadow-md lg:h-30">
-        <nav className="h-full flex items-center justify-between p-5 lg:max-w-[1277px] m-auto">
+        <nav className="h-full flex items-center justify-between p-5 lg:max-w-[1277px] m-auto lg:p-0">
           <Logo />
-          <Button className="w-[83] h-auto text-[10px] md:w-[147] md:text-sm">Iniciar sesión</Button>
+          <Button className="w-[83] h-auto text-[10px] md:w-[152] md:text-sm">Iniciar sesión</Button>
         </nav>
       </header>
   )
