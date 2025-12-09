c32a9facfe448ce6db01ca84bdb4570bd476f8c4
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-12-09 18:16:05 -0300

    No acceder a ruta testimonials/create sin permisos

--- frontend/testigo-app/src/app/(main)/testimonials/create/page.tsx
+++ frontend/testigo-app/src/app/(main)/testimonials/create/page.tsx
@@ -1,6 +1,29 @@
+'use client'
 import { TestimonialForm } from '@/components/testimonials/testimonial-form'
+import { useUserStore } from '@/store/userStore';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 
 const page = () => {
+  const router = useRouter();
+  const { token, rol, hasHydrated } = useUserStore();
+
+  useEffect(() => {
+    if (!hasHydrated) return; 
+
+    if (!token) {
+      router.replace("/login");
+      return;
+    }
+
+    if (rol !== "contributor") {
+      router.replace("/");
+      return;
+    }
+  }, [token, rol, hasHydrated, router]);
+
+
   return (
     <TestimonialForm />
   )
